256b22292e866b96a94e73026950c210087c02a7
ETS\ZZHENG <zzheng@ets.org>
2018-11-12 16:55:23 -0500

    first commit

---
+++ src/cmps/story.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './story.scss';
+
+const Story = ({ story, columns }) => {
+    const { title, url, author, num_comments, points } = story;
+
+    return (
+        <div className="story">
+            <span style={{ width: columns.title.width }}>
+                <a href={url}>{title}</a>
+            </span>
+            <span style={{ width: columns.author.width }}>
+                {author}
+            </span>
+            <span style={{ width: columns.comments.width }}>
+                {num_comments}
+            </span>
+            <span style={{ width: columns.points.width }}>
+                {points}
+            </span>
+            <span style={{ width: columns.archive.width }}>
+            </span>
+        </div>
+    )
+}
+
+export default Story;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './cmps/app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stories = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    }, {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+ReactDOM.render(<App stories={stories} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
