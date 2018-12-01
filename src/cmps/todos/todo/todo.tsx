4c305869e546e07bde9c332a7dc6c3809a2acd9b
Zeshi Zheng <mailtozeshi@gmail.com>
2018-11-30 19:57:21 -0500

    when todo completed give an animation

--- src/cmps/todos/todo/todo.tsx
+++ src/cmps/todos/todo/todo.tsx
@@ -4,26 +4,56 @@ import { ITodo } from '../../../models/todo';
 import { Dispatch } from 'redux';
 import { doToggleTodo } from '../../../actions/todos';
 import { connect } from 'react-redux';
+import { Spring } from 'react-spring';
 
 type Props = {
 	item: ITodo,
 	onToggle: (item: ITodo) => Dispatch
 };
 
-const Todo = ({item, onToggle}: Props) => {
-	return (
-		<div className="container-fluid border border-info">
-			<div className="row">
-				<div className="col-2">
-					<input type="checkbox" checked={item.completed} onChange={() => onToggle(item)} />
-				</div>
-				<div className="col">
-					<a href={item.link}><span className={item.completed ? 'crossed' : ''}>{item.name}</span></a>
-				</div>
-			</div>
-		</div>
-	);
-};
+type State = {
+	opacity: number
+}
+
+class Todo extends React.Component<Props, State> {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			opacity: 1
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle(item) {
+		this.setState({
+			opacity: 0
+		});
+		setTimeout(() => this.props.onToggle(item), 600);
+	}
+
+	render() {
+		const {item} = this.props;
+		return (
+			<Spring from={{opacity: 1}} to={this.state}>
+				{props =>
+					<div style={props}>
+						<div className="container-fluid border border-info">
+							<div className="row">
+								<div className="col-2">
+									<input type="checkbox" checked={item.completed} onChange={() => this.toggle(item)} />
+								</div>
+								<div className="col">
+									<a href={item.link}><span className={item.completed ? 'crossed' : ''}>{item.name}</span></a>
+								</div>
+							</div>
+						</div>
+					</div>}
+			</Spring>
+		);
+	}
+}
 
 const mapDispatchToProps = dispatch => {
 	return {
