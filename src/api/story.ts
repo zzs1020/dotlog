ba57bc66d08b3e9d0a026a4a74de94ad1df83bbf
Zeshi Zheng <mailtozeshi@gmail.com>
2018-12-03 22:47:13 -0500

    fixed ios api call issue

--- src/api/story.ts
+++ src/api/story.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ISearchResult } from '../models/search-result';
 
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
+const HN_BASE_URL = 'https://hn.algolia.com/api/v1/search?query=';
 
 const fetchStories = (query: string): Promise<ISearchResult> => (
 	axios.get(HN_BASE_URL + query).then((res) => res.data)
