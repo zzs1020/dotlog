a1fbbd9e13730940f39fccb49ee1701bba03fe5a
Zeshi Zheng <mailtozeshi@gmail.com>
2018-12-08 21:54:36 -0500

    filter out comments (non-story results)

--- src/actions/story.ts
+++ src/actions/story.ts
@@ -2,10 +2,14 @@ import { AxiosError } from 'axios';
 import { STORIES_ADD, STORIES_FETCH, HTTP_ERROR } from '../constants/action-types';
 import { IHit } from '../models/search-result';
 
-export const doAddStories = (stories: IHit[]) => ({
-	type: STORIES_ADD,
-	payload: stories
-});
+export const doAddStories = (hits: IHit[]) => {
+	// some hits are because finding keys on comments/authors, which may don't have a story
+	const stories = hits.filter(hit => hit.title);
+	return {
+		type: STORIES_ADD,
+			payload: stories
+	};
+};
 
 export const doFetchStories = (query: string) => ({
 	type: STORIES_FETCH,
