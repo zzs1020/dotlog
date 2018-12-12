db100b0273d3a9e52d0acc3e5f4803a001b4246e
Zeshi Zheng <mailtozeshi@gmail.com>
2018-12-11 21:52:41 -0500

    limit pagination to 7, also changed to vertical style

--- src/cmps/search-and-list/pagination/pagination.tsx
+++ src/cmps/search-and-list/pagination/pagination.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { IStoreState } from '../../../models/store-state.model';
 import { ISearchState } from '../../../models/search-state.model';
 import { doFetchStories } from '../../../actions/story.action';
+import './pagination.scss';
 
 type Props = {
 	currentSearch: ISearchState,
@@ -11,24 +12,44 @@ type Props = {
 
 const Pagination = ({ currentSearch, onFetchStories }: Props) => {
 	const { page, query, totalPages } = currentSearch;
+	const limitedPages = getLimitedPages(page + 1, totalPages);
 	return (
 		<nav>
-			<ul className="pagination">
-				<li className={`page-item ${page === 0 ? 'disabled' : ''}`} onClick={() => onFetchStories(query, page - 1)}>
-					<a className="page-link" href="#">&laquo;</a>
+			<ul className="my-pagination">
+				<li className={`my-page-item fa-rotate-90 ${page === 0 ? 'disabled' : ''}`} onClick={() => onFetchStories(query, page - 1)}>
+					&laquo;
 				</li>
 				{/** Array(number) only creates a new empty slots with length set up, no actual undefined value inserted */}
-				{(Array.apply(null, Array(totalPages))).map((_, i) =>
-					<li key={i} className={`page-item ${page === i ? 'active' : ''}`} onClick={() => onFetchStories(query, i)}>
-						<a className="page-link" href="#">{i + 1}</a>
+				{limitedPages.map(i =>
+					<li key={i} className={`my-page-item ${page === i - 1 ? 'active' : ''}`} onClick={() => onFetchStories(query, i)}>
+						{i}
 					</li>
 				)}
-				<li className={`page-item ${page === totalPages - 1 ? 'disabled' : ''}`} onClick={() => onFetchStories(query, page + 1)}>
-					<a className="page-link" href="#">&raquo;</a>
+				<li className={`my-page-item fa-rotate-90 ${page === totalPages - 1 ? 'disabled' : ''}`} onClick={() => onFetchStories(query, page + 1)}>
+					&raquo;
 				</li>
 			</ul>
 		</nav>
 	);
+};
+
+// here curPage start from 1 for convenience
+const getLimitedPages = (curPage, total) => {
+	let pages = [];
+	if (total >= 7) {
+		if (curPage > 4) {
+			for (let i = curPage - 3; i <= curPage + 3; i++) {
+				pages.push(i);
+			}
+		} else {
+			pages = [1, 2, 3, 4, 5, 6, 7];
+		}
+	} else if (total > 1) {
+		for (let i = 1; i <= total; i++) {
+			pages.push(i);
+		}
+	}
+	return pages;
 };
 
 const mapStateToProps = (state: IStoreState) => {
